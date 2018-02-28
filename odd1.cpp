6322daaa5ac195466f436ce282a6c227eb427713
aparna1997 <36396203+aparna1997@users.noreply.github.com>
2018-02-28 17:21:43 +0530

    odd1.cpp

---
+++ odd1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num1, num2, i, num;
+
+    cin >> num1;
+
+  cin >> num2;
+
+  cout << "odd numbers"<<num1 << " and " << num2 << " are: " << endl;
+  for(i = num1; i <= num2; i++)
+  {
+  	if(i%2!=0)
+  	cout<<i;
+  	
+  }
+
+  return 0;
+}
