e8f2ba0d7945b8bb45146ea19d96b6a788439355
aparna1997 <36396203+aparna1997@users.noreply.github.com>
2018-02-28 17:54:14 +0530

    multiplies.cpp

---
+++ multiplies.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+        int sum=0,i,n;
+      
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+        }
+        
+    return 0;
+    
+}
